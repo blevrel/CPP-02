8a1a1e9b3ef43b2d11e0579037a449ff174ddd7a
Baptist Levrel <blevrel@2A7.42angouleme.fr>
2023-01-16 10:23:59 +0100

    don

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: blevrel <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 11:53:24 by blevrel           #+#    #+#             */
-/*   Updated: 2022/12/16 13:33:43 by blevrel          ###   ########.fr       */
+/*   Updated: 2023/01/10 16:47:07 by blevrel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "Fixed.hpp"
@@ -14,14 +14,14 @@
 int main(void)
 {
 	Fixed a;
-	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	Fixed b( Fixed( 5.05f ) * Fixed( 2 ) );
 
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
 	std::cout << a << std::endl;
 	std::cout << a++ << std::endl;
 	std::cout << a << std::endl;
-	std::cout << b << std::endl;
+	std::cout << b / a << std::endl;
 	std::cout << Fixed::max( a, b ) << std::endl;
 	return (0);
 }
